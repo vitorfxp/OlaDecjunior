13939889da832f6dd10934f123d8c2da753cd35b
João Vitor Ferreira Ribeiro <joaovitorpvh777@gmail.com>
2025-04-02 10:53:43 -0400

    jav09

---
+++ src/devdojo/arraysmulti.java
@@ -0,0 +1,33 @@
+package devdojo;
+
+public class arraysmulti {
+    public static void main(String[] args) {
+        // meses
+        //dias
+        int [][] dias = new int [3][3];
+        dias[0][0] = 1;
+        dias[0][1] = 2;
+        dias[0][2] = 3;
+        dias[1][0] = 4;
+        dias[1][1] = 5;
+        dias[1][2] = 6;
+        dias[2][0] = 7;
+        dias[2][1] = 8;
+        dias[2][2] = 9;
+        for (int i = 0; i < dias.length; i++) {
+            for (int j = 0; j < dias[0].length; j++) {
+
+            }
+
+        }
+
+        for (int[]arr:dias){
+            for (int num:arr){
+                System.out.println(num);
+            }
+        }
+
+    }
+
+}
+
---
+++ src/devdojo/arraysmulti02.java
@@ -0,0 +1,22 @@
+package devdojo;
+
+public class arraysmulti02 {
+    public static void main(String[] args) {
+        int[][] arraysInt = new int[3][];
+
+        arraysInt [0] = new int []{1,2};
+        arraysInt [1] = new int []{1,2,46};
+        arraysInt [2] = new int []{1, 2, 4, 5, 6, 6};
+
+        for (int i = 0; i < arraysInt.length; i++) {
+            for (int j = 0; j < arraysInt[0].length; j++) {
+
+            }
+         for (int[]arr : arraysInt){
+             for (int num : arr){
+                 System.out.println(num);
+             }
+         }
+        }
+    }
+}
